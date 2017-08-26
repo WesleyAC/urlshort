5929d681bfd00dc0d9dadd1fdec67ce34d7e0419
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-08-25 23:41:16 -0400

    Add stats tracking to url redirect page

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import json
 
 app = Flask(__name__)
 
-url_map = {}
+url_map = []
+
 map_len = 0
 
 def make_slug(url):
@@ -12,36 +13,52 @@ def make_slug(url):
     map_len += 1
     return int2base(map_len, 36)
 
-def get_slug(url):
-    list(url_map.keys())[list(url_map.values()).index(url)]
+def find_item_by(type, value):
+    results = [i for i, v in enumerate(url_map) if v[type] == value]
+    if len(results) == 0:
+        return None
+    else:
+        return url_map[results[0]]
+
+def slug_exists(slug):
+    for item in url_map:
+        if item["slug"] == slug:
+            return True
+    return False
+
+def increment_views(slug):
+    slugs = [i for i, v in enumerate(url_map) if v["slug"] == slug]
+    if len(slugs) > 0:
+        url_map[slugs[0]]["views"] += 1
+
 
 @app.route("/")
 def index():
     return "Hello World!"
 
 @app.route("/urls", methods=["GET", "POST"])
-def urls():
+def urls_route():
     if request.method == "POST":
         if "url" in request.form:
-            if request.form["url"] not in url_map.values():
-                url_map[make_slug(request.form["url"])] = request.form["url"]
-            return Response(json.dumps({"url": request.form["url"], "slug": get_slug(request.form["url"])}), mimetype="application/json")
+            if not slug_exists(request.form["url"]):
+                url_map.append({
+                    "slug": make_slug(request.form["url"]),
+                    "url": request.form["url"],
+                    "views": 0})
+            return Response(json.dumps({"url": request.form["url"], "slug": find_item_by("url", request.form["url"])["slug"]}), mimetype="application/json")
         else:
             return Response("Error: `url` parameter is required.", mimetype="text/plain", status=500)
     else:
         return Response(json.dumps(url_map), mimetype="application/json")
 
 @app.route("/urls/<slug>")
-def get_url(slug):
-    response = {
-            "url": url_map[slug],
-            "slug": slug
-    }
-    return Response(json.dumps(response), mimetype="application/json")
+def get_url_route(slug):
+    return Response(json.dumps(find_item_by("slug", slug)), mimetype="application/json")
 
 @app.route("/r/<slug>")
-def redirect_url(slug):
-    return redirect(url_map[slug], code=301)
+def redirect_url_route(slug):
+    increment_views(slug)
+    return redirect(find_item_by("slug", slug)["url"], code=301)
 
 if __name__ == "__main__":
     app.run()
